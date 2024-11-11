8682ef83deaf946fcd5b88d2ebb23540a45f7c8c
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-11-11 16:04:40 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -163,3 +163,15 @@ function searchInTable(listId, searchInputId) {
 function confirmSearch(listId, searchInputId) {
     searchInTable(listId, searchInputId);
 }
+// Registrar o service worker quando a página for carregada
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch((error) => {
+            console.log('Falha ao registrar o Service Worker:', error);
+        });
+    });
+}
